26b2e583f2fb6fb92feeea6592fab7ff55218cca
Keerthie-22 <keerthie2233@gmailcom>
2024-08-29 12:14:40 +0530

    first commit

---
+++ ASSIGNMENT2/meaning-finder/src/App.js
@@ -0,0 +1,14 @@
+// src/App.js
+
+import React from 'react';
+import MeaningFinder from './MeaningFinder';
+
+function App() {
+  return (
+    <div className="App">
+      <MeaningFinder />
+    </div>
+  );
+}
+
+export default App;
